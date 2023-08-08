dea30b96b82a7917551c28ae7cc1c3fe7c2f06f6
eyobed101 <eyocjjazz101@gmail.com>
2023-08-08 15:28:18 +0300

    Sidebar component initial

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import Sidebar from './components/SideBar/Sidebar'
 
 function App() {
 
   return (
     <div className="app">
       <div className="app__body">
-        Test
+        <Sidebar />
       </div>
     </div>
   );
